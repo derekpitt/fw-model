f14371ea4da07a760a5725d4ceadc1f4bae876b2
Ryan Smith <rsmith@learningmachine.com>
2016-11-11 17:14:16 -0600

    0.1.6 - field validators should bail out if the input is null or empty

--- dist/validators.js
+++ dist/validators.js
@@ -18,15 +18,18 @@ function required(input) {
 var emailRegEx = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@((?=[a-z0-9-]{1,63}\.)(xn--)?[a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,63}$/i;
 
 function isEmail(input) {
+    if (input == null || input.length == 0) return null;
     return emailRegEx.test(input) ? null : "Not a valid Email Address";
 }
 
 function isNumber(input) {
+    if (input == null || input.length == 0) return null;
     var isNumeric = !isNaN(input - parseFloat(input));
     return isNumeric ? null : "Not a valid number";
 }
 
 function isInteger(input) {
+    if (input == null || input.length == 0) return null;
     var isInt = parseFloat(input) - parseInt(input) === 0;
     return isInt ? null : "Not a valid integer";
 }
@@ -34,5 +37,6 @@ function isInteger(input) {
 var urlRegEx = /^(https?:\/\/)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)$/;
 
 function isUrl(input) {
+    if (input == null || input.length == 0) return null;
     return urlRegEx.test(input) ? null : "Not a valid URL";
 }
--- src/validators.ts
+++ src/validators.ts
@@ -1,26 +1,29 @@
 export function required(input: string) {
   if (input == null || input.length == 0) return "Required";
-
   const hasValue = input.toString().replace(/^\s+/, "").replace(/\s+$/, "").length > 0;
   return hasValue ? null : "Required";
 }
 
 const emailRegEx = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@((?=[a-z0-9-]{1,63}\.)(xn--)?[a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,63}$/i;
 export function isEmail(input: string) {
+  if (input == null || input.length == 0) return null;
   return emailRegEx.test(input) ? null : "Not a valid Email Address";
 }
 
 export function isNumber(input: string) {
+  if (input == null || input.length == 0) return null;
   const isNumeric = !isNaN(<any>input - parseFloat(input));
   return isNumeric ? null : "Not a valid number";
 }
 
 export function isInteger(input: string) {
+  if (input == null || input.length == 0) return null;
   const isInt = parseFloat(input) - parseInt(input) === 0;
   return isInt ? null : "Not a valid integer";
 }
 
 const urlRegEx = /^(https?:\/\/)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)$/;
 export function isUrl(input: string) {
+  if (input == null || input.length == 0) return null;
   return urlRegEx.test(input) ? null : "Not a valid URL";
 }
